e515b72a6f7f86eb653cbde4fa98735a49b0fb24
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-05-26 12:10:02 -0500

    Created note on further development needs

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,24 +1,48 @@
 import { useState } from "react";
 import './searchBar.css';
+import jsonData from '../api';
 
 const SearchBar = () => {
 
-    const [userInput, setUserInput] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+    console.log(searchTerm);
 
-    const handleChange = (e) => {
-        setUserInput();
-        console.log(userInput);
-    }
 
+
+    // SEARCH WORKS BUT IS CURRENTLY ONLY SEARCHING NAMES.
+    // NEEDS TO WORK FOR DESCRIPTIONS AND PRICING
+    // ALSO NEEDS TO PULL THE DATA FROM THE TABLE, 
+    // NOT DIRECTLY FROM THE API
     return (
         <div>
             <input 
                 type="text" 
                 placeholder="search for an item"
-                value={userInput}
-                onChange={handleChange}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
                 className="searchBar"
             />
+            {jsonData.filter((val) => {
+                if (searchTerm === '') {
+                    return val;
+                } else if (
+                    val.name.toLowerCase().includes(searchTerm.toLowerCase())
+                ) {
+                    return val;
+                }
+            }).map((val, key) => {
+                return (
+                    <div className="item" key={key}>
+                        <p>{val.name}</p>
+                    </div>
+                )
+            })
+            
+            }
+
+
+
+
         </div>
     );
 }
