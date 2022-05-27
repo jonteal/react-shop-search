0bda13e0f682414da86b823cea9ab287bad8e5f9
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-05-27 16:58:09 -0500

    Refactored. Search works

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,17 +1,73 @@
-import Header from "../../components/Header/Header";
-import ProductList from "../../components/ProductList/ProductList";
+import { useEffect, useState } from "react";
+import apiData from '../../components/api';
+
 
 const Home = () => {
+
+    const [products, setProducts] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    
+
+    const FetchData = () => {
+        setProducts(apiData)
+    };
+
+    useEffect(() => {
+        FetchData();
+    }, []);
+
+
+    const convertCurrency = (money) => {
+        const converted = (money / 100).toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD',
+        });
+        return converted
+    }
+
+
+
     return (
         <div className="home-parent-container">
 
-            <div>
-                <Header />
-            </div>
+            <h1>Product Search</h1>
 
-            <div>
-                <ProductList />
-            </div>
+            <input 
+                type="text" 
+                placeholder="Search..."
+                onChange={(event) => setSearchTerm(event.target.value)}
+                className='searchBar'
+            />
+
+            <ul>
+                {products
+                    .filter((val) => {
+                        if (searchTerm === '') {
+                            return val;
+                        } else if (
+                            val.name.toLowerCase().includes(searchTerm.toLowerCase())
+                        ) {
+                            return val;
+                        } else if (
+                            val.description.toLowerCase().includes(searchTerm.toLowerCase())
+                        ) {
+                            return val;
+
+                        }
+                    })
+
+                    .map((product) => (
+                        <li key={product.id}>
+                            <div>
+                                <p>{product.name}</p>
+                                <p>{product.description}</p>
+                                <p>{convertCurrency(product.price)}</p>
+                            </div>
+                        </li>
+                    ))
+                }
+            </ul>
+
 
 
             
